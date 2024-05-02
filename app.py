19bb9220a4619a289c5d04cd920f9108dd830b6a
Sebastian Ong <s11082039@365stu.nutn.edu.tw>
2024-05-02 22:32:26 +0800

    Fetch data from firebase

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
-from flask import Flask
+from flask import Flask, jsonify
 import firebase_admin
 from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate("path/to/your/firebase/credentials.json")
+cred = credentials.Certificate(
+    "smartmailbox-8513f-firebase-adminsdk-gqlfr-9a1519f74f.json"
+)
 firebase_admin.initialize_app(cred)
-
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
 
 db = firestore.client()
 
+
+@app.route("/")
+def main_page():
+    return "Hello, World!"
+
 # Example route to fetch data from Firestore
-@app.route('/data')
+@app.route("/data")
 def get_data():
-    data = db.collection('your_collection').get()
+    data = db.collection("mails").get()
     # Process data as needed
-    return jsonify(data)
+    serialized_data = []
+    for doc in data:
+        serialized_data.append(doc.to_dict())
+    return jsonify(serialized_data)
