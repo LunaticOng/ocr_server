a55b40d2aa73a5ef2826035c27c04c1928efe115
Lunatic Ong <145681374+LunaticOng@users.noreply.github.com>
2024-05-03 20:51:09 +0800

    photos from firebase storage

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_file
 import firebase_admin
-from firebase_admin import credentials, firestore
+import tempfile
+from firebase_admin import credentials, firestore, storage
 
 app = Flask(__name__)
 
 # Initialize Firebase Admin SDK
-cred = credentials.Certificate(
-    "smartmailbox-8513f-firebase-adminsdk-gqlfr-9a1519f74f.json"
-)
-firebase_admin.initialize_app(cred)
+cred = credentials.Certificate("")
+firebase_app = firebase_admin.initialize_app(cred, {"storageBucket": ""})
 
 db = firestore.client()
 
@@ -16,6 +15,7 @@ db = firestore.client()
 @app.route("/")
 def main_page():
     return "Hello, World!"
+
 
 # Example route to fetch data from Firestore
 @app.route("/data")
@@ -26,3 +26,12 @@ def get_data():
     for doc in data:
         serialized_data.append(doc.to_dict())
     return jsonify(serialized_data)
+
+
+@app.route("/get_photo")
+def get_photo():
+    bucket = storage.bucket(app=firebase_app)
+    blob = bucket.blob("images/下載.png0e553576-fad1-487e-a6f5-912353f1fb74")
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    blob.download_to_filename(temp_file.name)
+    return send_file(temp_file.name, mimetype="image/jpeg")
