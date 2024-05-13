6d8c4056d9b0e154834afb826b5e1bbf41b57ac5
Sebastian Ong <s11082039@365stu.nutn.edu.tw>
2024-05-13 22:56:37 +0800

    ocr test succeed

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
+# Flask and firebase packages
+
 from flask import Flask, jsonify, send_file
 import firebase_admin
 import tempfile
 from firebase_admin import credentials, firestore, storage
+
+# OCR packages
+import easyocr
+import cv2
+
+# LLM packages
+# from ckip.transformers import CkipWordSegmenter, CkipPosTagger, CkipNerChunker
 
 app = Flask(__name__)
 
@@ -14,7 +23,7 @@ firebase_app = firebase_admin.initialize_app(
 db = firestore.client()
 
 
-@app.route("/")
+@app.route("/hw")
 def main_page():
     return "Hello, World!"
 
@@ -30,10 +39,22 @@ def get_data():
     return jsonify(serialized_data)
 
 
-@app.route("/get_photo")
+@app.route("/")
 def get_photo():
     bucket = storage.bucket(app=firebase_app)
     blob = bucket.blob("images/下載.png0e553576-fad1-487e-a6f5-912353f1fb74")
+    reader = easyocr.Reader(lang_list=['ch_tra', 'en'], gpu=False)
+    result = reader.readtext(blob.download_as_bytes())
     temp_file = tempfile.NamedTemporaryFile(delete=False)
+
+    myDict = {}
+    cnt = 0
+
+    for item in result:
+        myDict[cnt] = item[1]
+        cnt += 1
+
+    return jsonify(myDict)
+
     blob.download_to_filename(temp_file.name)
     return send_file(temp_file.name, mimetype="image/jpeg")
