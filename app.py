41c44107fa91fd40fdc7098b30732788659c43ff
Sebastian Ong <s11082039@365stu.nutn.edu.tw>
2024-06-13 15:31:53 +0800

    Timestamp added

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 import firebase_admin
-from firebase_admin import credentials, firestore, storage, initialize_app
+from firebase_admin import credentials, firestore, storage
 import easyocr
 import re
 import json
@@ -38,27 +38,33 @@ def get_photo():
     """Reads text from an image using EasyOCR and stores it in Firestore."""
     bucket = storage.bucket(app=firebase_app)
     image_blob = bucket.blob(
-        "test/images/no_personal_info.jpeg"
-    )  # replace with timestamp
+        "test/images/high_resolution.jpg"
+    )  # replace with the filename, which is passed with POST method
     image_data = image_blob.download_as_bytes()
 
+    # Initialize OCR reader
     reader = easyocr.Reader(lang_list=["ch_tra", "en"], gpu=False)
+    # Load the image and get the OCR results
     ocr_results = reader.readtext(image_data)
     ocr_res = [res[1] for res in ocr_results]
     ask_prompt = "\n".join(ocr_res)
     ask_prompt = (
-        "請告訴我這封信的內容，收件人(receiver)、寄件人(sender)、收件人地址(receiverAddress)、寄件人地址(senderAddress)、分類(type)、分爲高中低的緊急性(urgency)、如果有時間限制(timeLimit)可以告訴我詳細的時間，最後再給這封信一個合適的標題(title)，如果有任何未知的訊息，請以'未知'回覆，以Json格式回傳，並且key要用英文"
-        + "\n"
+        "請告訴我這封信的內容，收件人(receiver)、寄件人(sender)、收件人地址(receiverAddress)、寄件人地址(senderAddress)、分類(type)、分爲高中低的緊急性(urgency)、如果有時間限制(timeLimit)可以告訴我詳細的時間，最後再給這封信一個合適的標題(title)，如果有任何未知的訊息，請以'未知'回覆，以Json格式回傳，並且key要用英文\n"
         + ask_prompt
     )
 
+    # Get response from Gemini AI
     response = chat.send_message(ask_prompt)
 
     try:
         val = response.text
+        # Extract JSON data from response
         matcch = re.search(r"\{([^}]+)\}", val)
         matcch = json.loads("{" + matcch.group(1) + "}")
+        # Create timestamp
+        matcch["createAt"] = firestore.SERVER_TIMESTAMP
 
+        # Add data to Firestore
         db.collection("mailBoxes").document("test").collection("mails").add(matcch)
         return jsonify(matcch)
     except Exception as e:
